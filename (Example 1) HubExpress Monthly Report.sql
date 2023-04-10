c0f94eb617dcb4363e1776ef860579cca887490a
sbarnes880 <130379629+sbarnes880@users.noreply.github.com>
2023-04-10 12:45:12 -0400

    Update (Example 1) HubExpress Monthly Report.sql

--- (Example 1) HubExpress Monthly Report.sql
+++ (Example 1) HubExpress Monthly Report.sql
@@ -12,33 +12,33 @@ User		Date		Reason
 
 -- Access Rate
 
-SET NOCOUNT ON
-SET ANSI_WARNINGS OFF
+set nocount on
+set ansi_warnings off
 
 declare @start datetime set @start = dateadd(mm,datediff(mm,0,getdate())-13,0)
 declare @end datetime set @end = dateadd(mm,datediff(mm,0,getdate())-1,0)
 
-SELECT 
+select 
       request_month AS 'Month',
       drug_name,
-      COUNT (DISTINCT request_id_mask) AS 'PA Volume',
-      COUNT(DISTINCT CASE WHEN revenue_source_category ='Pharmacy' AND accessed_online = 1 THEN request_id_mask
-      WHEN revenue_source_category = 'Physician' THEN request_id_mask ELSE NULL END) AS 'Accessed'
-      SUM (sent_to_plan) AS 'Submitted',
-      SUM (known_outcome) AS 'Known Outcome',
-      SUM (approved) AS 'Approved',
+      count (distinct request_id_mask) AS 'PA Volume',
+      count (distinct case when revenue_source_category ='Pharmacy' and accessed_online = 1 then request_id_mask
+      when revenue_source_category = 'Physician' then request_id_mask else null end) as 'Accessed'
+      sum (sent_to_plan) as 'Submitted',
+      sum (known_outcome) as 'Known Outcome',
+      sum (approved) as 'Approved',
       lob,
-      pa_type AS 'PA Type',
-      rejection_code AS 'Reject Code',
+      pa_type as 'PA Type',
+      rejection_code as 'Reject Code',
       revenue_source_category,
       revenue_source
-FROM pharma_mart..vw_tableau_product_reporting
-WHERE request_month between @start and @end
+from pharma_mart..vw_tableau_product_reporting
+where request_month between @start and @end
 and drug_name in ('[brand name]')
-AND is_appeal = 0
-GROUP BY 
+and is_appeal = 0
+group by 
       request_month,drug_name,
       revenue_source_category
-ORDER BY 
+order by
       'Month', 
       drug_name  

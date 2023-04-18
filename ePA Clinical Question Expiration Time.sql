3f0f740b031937d0b7907b9529cabc0d37742ca8
sbarnes880 <130379629+sbarnes880@users.noreply.github.com>
2023-04-18 09:29:08 -0600

    Update ePA Clinical Question Expiration Time.sql

--- ePA Clinical Question Expiration Time.sql
+++ ePA Clinical Question Expiration Time.sql
@@ -15,8 +15,8 @@ select
 	month
 	,cast(epa_ques_resp as date) as Questions_Rec_Date
 	,cast(epa_ques_resp as time) as Questions_Rec_Time
-	,cast(left(field_value,10) as date) as Questions_Exp_Date
-	,cast(left(right(field_value,len(field_value)-CHARINDEX('t',field_value)),8) as time) as Questions_Exp_Time
+	,cast(left(field_value,10) as date) as Questions_Exp_Date -- Tells SQL to only grab the left 10 digits into the field_value record to interpret the date.
+	,cast(left(right(field_value,len(field_value)-CHARINDEX('t',field_value)),8) as time) as Questions_Exp_Time -- Tells SQL to only grab the right 8 digits into the field_value record to interpret the time.
 	,count(distinct m.request_id) as PA_Vol
 from plan_pbm..t_monthly_reporting_data m
 join epamotron_repl..epa_reporting_fields e on m.request_id=e.request_id
